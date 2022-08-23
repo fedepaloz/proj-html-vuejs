74a6ceb34664bc295a768b80ed5133a6302c8fb0
fedepaloz <palozzifederico@gmail.com>
2022-08-23 17:13:56 +0200

    installato fontawesome, aggiunte le immagini, sass

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <div></div>
+  <div>
+<BaseHeader></BaseHeader>
+  </div>
 </template>
 
 <script>
+
+import BaseHeader from './components/BaseHeader.vue';
+
 
 
 export default {
   name: 'App',
   components: {
-   
-  }
+    BaseHeader
+}
 }
 </script>
 
 <style lang="scss">
 
+@import'./sass/style.scss'
 </style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,21 @@
+<template>
+<header>
+    <div class="row d-flex">
+        <div class="col-6"><img src="../assets/avada-img/avada-logo/avada-music-logo.png" alt=""></div>
+       <font-awesome-icon icon="fa-solid fa-bars" />
+    </div>
+</header>
+</template>
+
+<script>
+
+export default {
+name:"BaseHeader"
+};
+</script>
+
+<style lang="scss">
+
+header{width: 100%;
+background-color: grey;}
+</style>
