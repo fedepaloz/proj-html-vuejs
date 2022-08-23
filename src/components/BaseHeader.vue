6321b1d190b48a36bb5a1cad913cb4b5cd2457de
fedepaloz <palozzifederico@gmail.com>
2022-08-23 18:36:15 +0200

    icons added

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,21 +1,24 @@
 <template>
-<header>
+  <header>
     <div class="row d-flex">
-        <div class="col-6"><img src="../assets/avada-img/avada-logo/avada-music-logo.png" alt=""></div>
-       <font-awesome-icon icon="fa-solid fa-bars" />
+      <div class="col-6">
+        <img src="../assets/avada-img/avada-logo/avada-music-logo.png" alt="" />
+      </div>
+      <font-awesome-icon icon="fa-solid fa-bars" />
+      
     </div>
-</header>
+  </header>
 </template>
 
 <script>
-
 export default {
-name:"BaseHeader"
+  name: "BaseHeader",
 };
 </script>
 
 <style lang="scss">
-
-header{width: 100%;
-background-color: grey;}
+header {
+  width: 100%;
+  background-color: grey;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faBars)
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import{faInstagram} from '@fortawesome/free-brands-svg-icons'
+import{faFacebook} from '@fortawesome/free-brands-svg-icons'
+import{faTwitter}from '@fortawesome/free-brands-svg-icons'
+import{faYoutube} from '@fortawesome/free-brands-svg-icons'
+library.add(faBars,faInstagram,faFacebook,faTwitter,faYoutube)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
+
+
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
