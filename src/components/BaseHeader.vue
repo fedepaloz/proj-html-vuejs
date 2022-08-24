b2690e6f2d84a9925edb50daec3fd0cc86f1ccbf
fedepaloz <palozzifederico@gmail.com>
2022-08-24 21:45:51 +0200

    header fixed (props yet missing)

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,25 +1,26 @@
 <template>
   <header>
-    <div class="navbar">
+    <div v-if="!visible" class="navbar">
       <div class="row justify-content-between">
         <div class="col-4"><BaseLogo></BaseLogo></div>
         <div class="col-1 bars">
-          <button @click="awesome = true" class="openbtn">
+          <button @click="visible = true" class="openbtn">
             <font-awesome-icon icon="fa-solid fa-bars" />
           </button>
         </div>
       </div>
     </div>
-    <div v-if="awesome" id="menu" class="text-white hidden">
-      <h1>menu</h1>
-      <ul>
-        <li>lol</li>
-        <li>lol</li>
-        <li>lol</li>
-        <li>lol</li>
-        <li>lol</li>
-        <li>lol</li>
-      </ul>
+    <div v-if="visible" id="menu" class="text-white">
+      <div class="container-fluid text-center">
+        <ul class="menu-list">
+          <li><h2>Home</h2></li>
+          <li><h2>Meet the band</h2></li>
+          <li><h2>live dates</h2></li>
+          <li><h2>latest news</h2></li>
+          <li><h2>albums</h2></li>
+          <li><h2>fans</h2></li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
@@ -27,19 +28,18 @@
 <script>
 import BaseLogo from "./BaseLogo.vue";
 export default {
-   data(){
-  return {
-    awesome: false
-  }
-  
-},
+  data() {
+    return {
+      visible: true,
+    };
+  },
   name: "BaseHeader",
   components: { BaseLogo },
-  
 };
 </script>
 
 <style lang="scss">
+@import "../sass/vars";
 header {
   background-color: #262833;
   position: fixed;
@@ -49,7 +49,14 @@ header {
 .bars {
   color: white;
 }
- #menu{
-  
- }
+
+#menu {
+  height: 100vh;
+  background-color: $mandy;
+}
+
+.menu-list {
+  list-style-type: none;
+  padding-top: 300px;
+}
 </style>
