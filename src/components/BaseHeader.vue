c0ee7056662e666a4a112ac7c33b80699ef8669c
fedepaloz <palozzifederico@gmail.com>
2022-08-24 19:09:34 +0200

    menu navbar set

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,20 +1,41 @@
 <template>
   <header>
-    <div class="row justify-content-between">
-      <div class="col-4"><BaseLogo></BaseLogo></div>
-      <div class="col-1 bars">
-        <button><font-awesome-icon  icon="fa-solid fa-bars" /></button>
+    <div class="navbar">
+      <div class="row justify-content-between">
+        <div class="col-4"><BaseLogo></BaseLogo></div>
+        <div class="col-1 bars">
+          <button @click="awesome = true" class="openbtn">
+            <font-awesome-icon icon="fa-solid fa-bars" />
+          </button>
+        </div>
       </div>
     </div>
-    
+    <div v-if="awesome" id="menu" class="text-white hidden">
+      <h1>menu</h1>
+      <ul>
+        <li>lol</li>
+        <li>lol</li>
+        <li>lol</li>
+        <li>lol</li>
+        <li>lol</li>
+        <li>lol</li>
+      </ul>
+    </div>
   </header>
 </template>
 
 <script>
 import BaseLogo from "./BaseLogo.vue";
 export default {
+   data(){
+  return {
+    awesome: false
+  }
+  
+},
   name: "BaseHeader",
   components: { BaseLogo },
+  
 };
 </script>
 
@@ -28,4 +49,7 @@ header {
 .bars {
   color: white;
 }
+ #menu{
+  
+ }
 </style>
