be3cde9e117cbaf6f45cfba3294abc369d8a2839
fedepaloz <palozzifederico@gmail.com>
2022-08-25 11:38:22 +0200

    view menu added

---
+++ src/components/ViewMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="view-menu">
+    <h3 class="text-white text-center py-5">VIEW ALL LATEST NEWS</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../sass/vars";
+#view-menu {
+  background-color: $mandy;
+}
+</style>
