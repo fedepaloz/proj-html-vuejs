f4a314187b5e7f02793f509e69e80e8f2bd34f04
fedepaloz <palozzifederico@gmail.com>
2022-08-25 15:04:24 +0200

    jumbotron buttons hover added

--- src/components/BaseJumbo.vue
+++ src/components/BaseJumbo.vue
@@ -3,9 +3,11 @@
     <div class="container-fluid">
       <div class="row flex-column align-items-center">
         <div class="col text-white text-center">
-          <h1>{{mainTitle}}</h1>
+          <h1>{{ mainTitle }}</h1>
           <h4>here is an untold story behind every favorite song.</h4>
-          <button type="button" class="btn btn-dark">Latest Album</button><button type="button" class="btn btn-dark">Live Dates</button>
+          <button type="button" class="button mx-2" id="btnsx">
+            Latest Album</button
+          ><button type="button" class="button mx-2" id="btndx">Live Dates</button>
         </div>
       </div>
     </div>
@@ -14,17 +16,26 @@
 
 <script>
 export default {
-  props:{
-    mainTitle: String
-
-  }
+  props: {
+    mainTitle: String,
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "../sass/vars";
 .jumbotron {
   background-image: url(../assets/avada-img/home_slider.jpg);
   height: 100vh;
   margin-top: 90px;
+}
+#btndx{
+  background-color:transparent;
+  border: 2px solid white;
+  
+}
+#btndx:hover,#btnsx:hover{
+  background-color:white ;
+  color: black;
 }
 </style>
